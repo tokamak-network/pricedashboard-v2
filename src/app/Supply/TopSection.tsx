e91cca2d10df07a69061fd73ab52dac9afec7b59
SonYoungsung <sysgigigi@gmail.com>
2024-08-25 18:32:06 +0900

    feat : add icon for stakedTON

--- src/app/Supply/TopSection.tsx
+++ src/app/Supply/TopSection.tsx
@@ -1,63 +1,93 @@
-import { Flex, useMediaQuery } from '@chakra-ui/react'
-import SectionTitle from './components/Title';
-import Supply from './components/Supply';
-import Burnt from './components/Burnt';
+import { Flex, useMediaQuery, Link, Text } from "@chakra-ui/react";
+import SectionTitle from "./components/Title";
+import Supply from "./components/Supply";
+import Burnt from "./components/Burnt";
+import LINK_ICON from "@/assets/foreign.svg";
+import Image from "next/image";
 
-function TopSection (props: { type: string }) {
-  const { type } = props
+function TopSection(props: { type: string }) {
+  const { type } = props;
   const [isMobile] = useMediaQuery("(max-width: 920px)");
-  
+
   return (
     <Flex
-      w={isMobile ? '330px' : '450px'}
-      borderRadius={'10px'}
-      mx={isMobile ? '10px' : ''}
-      mb={
-        isMobile ? 
-        type === 'supply' ? '20px' : '120px' 
-        : ''
-      }
+      w={isMobile ? "330px" : "450px"}
+      borderRadius={"10px"}
+      mx={isMobile ? "10px" : ""}
+      mb={isMobile ? (type === "supply" ? "20px" : "120px") : ""}
       height={
-        isMobile ?
-        type === 'supply' ? '410px' : '214px': 
-        type === 'supply' ? '330px' : '184px'
+        isMobile
+          ? type === "supply"
+            ? "410px"
+            : "214px"
+          : type === "supply"
+            ? "330px"
+            : "184px"
       }
-      bg={'#eceef9'}
-      boxShadow={'1px 1px 5px 0px rgba(0, 0, 0, 0.10) inset'}
+      bg={"#eceef9"}
+      boxShadow={"1px 1px 5px 0px rgba(0, 0, 0, 0.10) inset"}
       // padding={'10px'}
       // mr={'20px'}
-      flexDir={'column'}
-      alignItems={'center'}
-      justifyContent={'start'}
+      flexDir={"column"}
+      alignItems={"center"}
+      justifyContent={"start"}
     >
-      <SectionTitle 
-        title={ type === 'supply' ? 'TON SUPPLY' : 'TON LOCKED'}
-      />
+      <SectionTitle title={type === "supply" ? "TON SUPPLY" : "TON LOCKED"} />
       <Flex
-        // mt={'28px'}
+      // mt={'28px'}
       >
-        {
-          type === 'supply' ? (
-            <Supply />
-          ) : (
-            <Flex flexDir={'column'}>
-              <Burnt />
-              <Flex flexDir={'column'} mt={isMobile ? '30px' : '60px'}>
-                <Flex fontSize={'13px'} fontWeight={300} w={isMobile ? '330px' : '396px'} mb="15px">
-                  † Contract values need to be divided by 10^27 to get correct decimal place.
-                </Flex>
-                <Flex fontSize={'13px'} fontWeight={300} w={isMobile ? '330px' : '396px'} mb="23px">
-                  †† Contract values need to be divided by 10^18 to get correct decimal place.
-                </Flex>
+        {type === "supply" ? (
+          <Supply />
+        ) : (
+          <Flex flexDir={"column"}>
+            <Burnt />
+            <Flex flexDir={"column"} mt={isMobile ? "30px" : "35px"}>
+              <Flex
+                fontSize={"13px"}
+                fontWeight={300}
+                w={isMobile ? "330px" : "396px"}
+                mb="15px"
+                alignItems={"baseline"}
+              >
+                <Link
+                  mr={"5px"}
+                  href={
+                    "https://etherscan.io/address/0x0b55a0f463b6defb81c6063973763951712d0e5f#readProxyContract#F59"
+                  }
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Image src={LINK_ICON} alt="" />
+                </Link>
+                <Text m={0}>
+                  Verify this value on Etherscan. In case of discrepancies,
+                  Etherscan's value is considered accurate.
+                </Text>
+              </Flex>
+              <Flex
+                fontSize={"13px"}
+                fontWeight={300}
+                w={isMobile ? "330px" : "396px"}
+                mb="15px"
+              >
+                † Contract values need to be divided by 10^27 to get correct
+                decimal place.
+              </Flex>
+              <Flex
+                fontSize={"13px"}
+                fontWeight={300}
+                w={isMobile ? "330px" : "396px"}
+                mb="23px"
+              >
+                †† Contract values need to be divided by 10^18 to get correct
+                decimal place.
               </Flex>
             </Flex>
-          )
-        }
-
+          </Flex>
+        )}
       </Flex>
-
     </Flex>
-  )
+  );
 }
 
-export default TopSection
+export default TopSection;
